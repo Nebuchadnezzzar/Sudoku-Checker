a943e1c7785d541b358be4a797b8c0c2b5c7bab9
Nebuchadnezzzar <przemyslaw.szalk@gmail.com>
2018-07-11 12:42:24 +0200

    comments added

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,25 @@
-# Comments needed
+# Sudoku Validator
+# A tiny program validating a traditional 9x9 Sudoku. 
+# Sudoku requires that every row, column and 3x3 square contain
+# 1, 2, 3, 4, 5, 6, 7, 8, and 9 numbers. As no duplicates are allowed,
+# no rows, columns or regions can be identical. 
+# The program accepts Python matrix with 9 lists consisting of 9 elements
+# as an input.
+
 def done_or_not(board):
 	matrix = list(range(1,10))
-	# rows
+	# validating rows
 	for row in board:
 		if sorted(row) != matrix:
 			return "Try again!"
-	# columns
+	# validating columns
 	for pos in range(len(board)):
 		column = []
 		for row in board:
 			column.append(row[pos])	
 		if sorted(column) != matrix:
 			return "Try again!"
-	# squares
+	# validating squares 3x3
 	for third in range(0,9,3):
 		for i in range(1,4,1):
 			square = []
@@ -29,7 +36,8 @@ def done_or_not(board):
 			if sorted(square) != matrix:
 				return "Try again!"		
 	return "Finished!"
-	
+
+# test inputs	
 board = [[1, 3, 2, 5, 7, 9, 4, 6, 8]
 ,[4, 9, 8, 2, 6, 1, 3, 7, 5]
 ,[7, 5, 6, 3, 8, 4, 2, 1, 9]
